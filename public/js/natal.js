0acfa44bb7e9b5f83cdbf870a074f5f6bdc15b4d
pedrooliveira-po <pedrodeoliveirapo123@proton.me>
2025-12-05 13:49:59 +0100

    Delete public/js directory

--- public/js/natal.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hoje = new Date()
-    const mes = hoje.getMonth()
-    if (mes == 11) {
-        const logo = document.querySelector('#logotipo');
-        logo.src = '/assets/logos/prologNatal.png'
-    }
-})
